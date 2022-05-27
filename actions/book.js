74f1686597064dc7050bc008fd8719369c691c75
mosharif30 <sharifkhani.mr@gmail.com>
2022-05-27 22:50:31 +0430

    add api

--- actions/book.js
+++ actions/book.js
@@ -3,7 +3,7 @@ import reduxCall from '../utils/reduxCall'
 const GET_LIST_BOOK_ACTION = () => {
   return async (dispatch) => {
     return reduxCall(dispatch, {
-      url: 'http://127.0.0.1:5000/books.json',
+      url: 'https://book-api-mohammadreza.herokuapp.com/books',
       method: 'GET',
       name: 'GET_LIST_BOOK',
     })
